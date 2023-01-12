a0f8e79d5aa5f09b3040eab95970ebf01a61e965
unknown <andryratsimba@gmail.com>
2023-01-12 20:58:52 +0100

    added token refresh automatic programming

--- server/server.js
+++ server/server.js
@@ -8,25 +8,48 @@ const refreshGameData = require('./datafetchers/gameDataFetcher');
 const refreshGameModesData = require('./datafetchers/gameModesDataFetcher');
 const refreshGameGenresData = require('./datafetchers/gameGenresDataFetcher');
 
-// Setting axios config to request from IGDB API
-axios.defaults.headers.post['Client-ID'] = process.env.IGDB_CLIENT_ID;
-axios.defaults.headers.post['Authorization'] = `Bearer ${process.env.IGDB_TEMPORARY_TOKEN}`;
-
 // Loading data in memory
 let gamedata = jsonfile.readFileSync('./tmp/gamedata.json');
 let genredata = jsonfile.readFileSync('./tmp/gamegenresdata.json');
 let gamemodesdata = jsonfile.readFileSync('./tmp/gamemodesdata.json');
 
-// Twitch API token function
-// function getTwitchAPIToken () {
-// 	axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`)
-// 		.then(res=>{
-// 			console.log(res.data)
-// 		})
-// 		.catch(err=>{
-// 			console.log(err)
-// 		})
-// };
+// Setting axios config to request from IGDB API
+axios.defaults.headers.post['Client-ID'] = process.env.IGDB_CLIENT_ID;
+
+getIGDBAPIToken();
+
+// Getting IGDB token an programming the next token fetching
+function getIGDBAPIToken () {
+	axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`)
+		.then(res=>{
+			process.env.IGDB_TEMPORARY_TOKEN = res.data.access_token;
+			axios.defaults.headers.post['Authorization'] = `Bearer ${res.data.access_token}`;
+			programNextTokenRefresh();
+		})
+		.catch(err=>{
+			console.log(err)
+		})
+};
+
+const tokenRefreshInterval = 59 * 24 * 60 * 60 * 1000;
+
+function timeout(ms) {
+  setTimeout(() => {
+    getIGDBAPIToken();
+  }, ms);
+}
+
+function programNextTokenRefresh() {
+  let timeRemaining = tokenRefreshInterval;
+  let interval = 2147483647; // maximum time that can be set for setTimeout
+  (function recursiveTimeout() {
+    if (timeRemaining > 0) {
+      timeout(Math.min(interval, timeRemaining));
+      timeRemaining -= interval;
+      setTimeout(recursiveTimeout, Math.min(interval, timeRemaining));
+    }
+  })();
+}
 
 // Programming the next data refresh on next thursday a 9 am (time of GFN games availability release)
 const now = new Date();
@@ -81,7 +104,6 @@ async function refreshData () {
 // Serving the front-end app
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
-// Implementing CORS
 app.use(cors());
 
 // API Routes
